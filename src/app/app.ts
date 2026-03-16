1f3ae4a76876a595fb7f0abd05be30ac03afb526
Faiz Ahmed Amine <faizahmedamine2004@gmail.com>
2026-03-16 02:37:32 +0100

    initial angular frontend

---
+++ src/app/app.ts
@@ -0,0 +1,21 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Api } from './services/api';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.html'
+})
+export class App implements OnInit {
+  message = '';
+
+  constructor(private api: Api) {}
+
+  ngOnInit(): void {
+    this.api.getHello().subscribe({
+      next: data => this.message = data,
+      error: err => this.message = 'Error connecting to API'
+    });
+  }
+}
