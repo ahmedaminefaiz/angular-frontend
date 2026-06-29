3ba471126861393dceddac371d76bc37a83b2d0d
ahmedaminefaiz <150074739+ahmedaminefaiz@users.noreply.github.com>
2026-06-29 17:42:07 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ebd2-springbootappprod-0yzlxj-8fd11f-192-166-204-204.traefik.me/api'
+  apiUrl: '/api'  // relatif — le browser appelle le même domaine
 };
